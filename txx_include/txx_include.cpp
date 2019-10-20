852300d6818deb7fa443c15e187850ac4c49a876
l82as <l82as@mail.ru>
2019-10-20 11:09:30 +0400

    dell  garbage comments 2

--- txx_include/txx_include.cpp
+++ txx_include/txx_include.cpp
@@ -1,7 +1,4 @@
-﻿// txx_include.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
 #include <cmath>
 #include "TX/TXLib.h"
